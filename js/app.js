bbdf2c291cc68cbc7c6149d7f231236d6eddaf64
unknown <alex.chinchi02@gmail.com>
2024-10-17 19:50:47 +0200

    sacadita

--- js/app.js
+++ js/app.js
@@ -2,8 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     // VARIABLES
     let kb = document.querySelectorAll(".text-back");
     let hbInicio = document.querySelector("#hb");
+    let headerElc1 = document.querySelector(".enlaces > a:nth-child(1)");
+    let headerElc2 = document.querySelector(".enlaces > a:nth-child(2)");
 
     kevinBacon();
+
+    // EVENTOS
+    headerElc1.addEventListener("mouseenter", () => {
+        pHeaderA1.classList.add("animHover")
+    });
+
+    headerElc2.addEventListener("mouseenter", () => {
+        pHeaderA2.classList.add("animHover")
+    });
 
     // FUNCIONES 
     function kevinBacon(){
@@ -18,5 +29,9 @@ document.addEventListener("DOMContentLoaded", () => {
             el.style.top = top+"%";
             top+=13;
         });
+    }
+
+    function animHoverEnlaces(){
+        
     }
 })
