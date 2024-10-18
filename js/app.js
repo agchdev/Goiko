ce0062e0c1cf4a8d86dc40cb35e61cb7ac364298
unknown <alex.chinchi02@gmail.com>
2024-10-18 12:03:37 +0200

    transiciones

--- js/app.js
+++ js/app.js
@@ -4,16 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
     let hbInicio = document.querySelector("#hb");
     let headerElc1 = document.querySelector(".enlaces > a:nth-child(1)");
     let headerElc2 = document.querySelector(".enlaces > a:nth-child(2)");
+    let pHeaderA1 = document.querySelectorAll(".enlaces > a:nth-child(1) > div > span");
+    let pHeaderA2 = document.querySelectorAll(".enlaces > a:nth-child(2) > div > span");
 
     kevinBacon();
 
     // EVENTOS
     headerElc1.addEventListener("mouseenter", () => {
-        pHeaderA1.classList.add("animHover")
+        pHeaderA1.forEach(el => {
+            el.classList.add("animHover");
+            setTimeout(() => {
+                el.classList.remove("animHover");
+            }, 399);
+        });
     });
 
     headerElc2.addEventListener("mouseenter", () => {
-        pHeaderA2.classList.add("animHover")
+        pHeaderA2.forEach(el => {
+            el.classList.add("animHover");
+            setTimeout(() => {
+                el.classList.remove("animHover");
+            }, 399);
+        });
     });
 
     // FUNCIONES 
@@ -28,9 +40,14 @@ document.addEventListener("DOMContentLoaded", () => {
         kb.forEach(el => {
             el.style.top = top+"%";
             top+=13;
+            el.classList.add("introKB");
+            setTimeout(() => {
+                el.classList.remove("introKB");
+                el.classList.add("animKB");
+            }, 2000);
         });
     }
-
+    
     function animHoverEnlaces(){
         
     }
